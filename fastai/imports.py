34b86c8d1e6394099247233e2356b8a943c10809
Ubuntu <ubuntu@ip-172-31-18-92.ec2.internal>
2018-07-19 16:07:22 +0000

    Removed seaborn from imports.py

--- fastai/imports.py
+++ fastai/imports.py
@@ -1,7 +1,7 @@
 from IPython.lib.deepreload import reload as dreload
 import PIL, os, numpy as np, math, collections, threading, json, bcolz, random, scipy, cv2
 import pandas as pd, pickle, sys, itertools, string, sys, re, datetime, time, shutil, copy
-import seaborn as sns, matplotlib
+import matplotlib
 import IPython, graphviz, sklearn_pandas, sklearn, warnings, pdb
 import contextlib
 from abc import abstractmethod
